bad538fdee8531b97d6c7a819335d1d302e14936
aaravm <aarav.mehta.mat22@itbhu.ac.in>
2024-04-14 12:26:12 +0530

    added txid maker for p2wpkh

--- foo/src/main.rs
+++ foo/src/main.rs
@@ -4,11 +4,10 @@ extern crate hex;
 use secp256k1::{Secp256k1, Message, PublicKey, Signature, Error};
 use hex::{decode, FromHex};
 
-use std::{fs::File, os::linux::raw};
+use std::fs::File;
 use std::io::Read;
 use sha2::{Digest, Sha256};
 use std::str;
-use std::path::Path;
 use std::fs;
 
 fn hex_to_little_endian(hex_number: &str) -> String {
@@ -18,40 +17,40 @@ fn hex_to_little_endian(hex_number: &str) -> String {
     hex::encode(little_endian_bytes)
 }
 
-fn create_transaction_p2wpkh(data: serde_json::Value, parameter: usize, flag: &mut bool) -> String {
+fn create_transaction_p2wpkh(data: serde_json::Value, parameter: usize) -> String {
     let mut raw_transaction = String::new();
 
     let version = format!("{:08x}", data["version"].as_u64().unwrap());
     raw_transaction += &hex_to_little_endian(&version);
 
-    let mut hashPrevouts = String::new();
+    let mut hash_prevouts = String::new();
 
     for input in data["vin"].as_array().unwrap() {
         let prev_txid = input["txid"].as_str().unwrap();
-        hashPrevouts += &hex_to_little_endian(prev_txid);
+        hash_prevouts += &hex_to_little_endian(prev_txid);
 
         let prev_index = format!("{:08x}", input["vout"].as_u64().unwrap());
-        hashPrevouts += &hex_to_little_endian(&prev_index);
+        hash_prevouts += &hex_to_little_endian(&prev_index);
     }
 
     // println!("hashPrevouts is: {}",hashPrevouts);
-    let data1 = Vec::from_hex(hashPrevouts).unwrap();
+    let data1 = Vec::from_hex(hash_prevouts).unwrap();
     let data2 = Sha256::digest(&data1).to_vec();
     let data3 = Sha256::digest(&data2).to_vec();
     let data4 = hex::encode(data3);
 
     raw_transaction += &data4;
 
-    let mut hashSequence = String::new();
+    let mut hash_sequence = String::new();
 
     for input in data["vin"].as_array().unwrap() {
         let sequence_decimal = input["sequence"].as_u64().unwrap_or_default();
         let sequence_hex = format!("{:08x}", sequence_decimal);
-        hashSequence += &hex_to_little_endian(&sequence_hex);
+        hash_sequence += &hex_to_little_endian(&sequence_hex);
     }
 
     // println!("hashSequence is: {}",hashSequence);
-    let data1 = Vec::from_hex(hashSequence).unwrap();
+    let data1 = Vec::from_hex(hash_sequence).unwrap();
     let data2 = Sha256::digest(&data1).to_vec();
     let data3 = Sha256::digest(&data2).to_vec();
     let data4 = hex::encode(data3);
@@ -75,20 +74,20 @@ fn create_transaction_p2wpkh(data: serde_json::Value, parameter: usize, flag: &m
     let sequence_hex = format!("{:08x}", sequence_decimal);
     raw_transaction += &hex_to_little_endian(&sequence_hex);
 
-    let mut hashOutputs = String::new();
+    let mut hash_outputs = String::new();
 
     for output in data["vout"].as_array().unwrap() {
         let value = format!("{:016x}", (output["value"].as_f64().unwrap()) as u64);
-        hashOutputs += &hex_to_little_endian(&value);
+        hash_outputs += &hex_to_little_endian(&value);
 
         let temp = output["scriptpubkey"].as_str().unwrap().len() / 2;
 
-        hashOutputs += &hex_to_little_endian(&format!("{:02x}", temp));
-        hashOutputs += output["scriptpubkey"].as_str().unwrap();
+        hash_outputs += &hex_to_little_endian(&format!("{:02x}", temp));
+        hash_outputs += output["scriptpubkey"].as_str().unwrap();
 
     }
 
-    let data1 = Vec::from_hex(hashOutputs).unwrap();
+    let data1 = Vec::from_hex(hash_outputs).unwrap();
     let data2 = Sha256::digest(&data1).to_vec();
     let data3 = Sha256::digest(&data2).to_vec();
     let data4 = hex::encode(data3);
@@ -111,8 +110,62 @@ fn create_transaction_p2wpkh(data: serde_json::Value, parameter: usize, flag: &m
     hex
 }
 
+fn create_transaction_p2wpkh_final(data: serde_json::Value) -> String {
+    let mut raw_transaction = String::new();
 
-fn create_transaction_p2pkh(data: serde_json::Value, parameter: usize, flag: &mut bool) -> String {
+    let version = format!("{:08x}", data["version"].as_u64().unwrap());
+    raw_transaction += &hex_to_little_endian(&version);
+
+    let input_count = format!("{:02x}", data["vin"].as_array().unwrap().len());
+    raw_transaction += &hex_to_little_endian(&input_count);
+
+    let mut ind: usize = 0;
+
+    for input in data["vin"].as_array().unwrap() {
+        
+        // println!("index is {}",ind);
+        let prev_txid = input["txid"].as_str().unwrap();
+        raw_transaction += &hex_to_little_endian(prev_txid);
+
+        let prev_index = format!("{:08x}", input["vout"].as_u64().unwrap());
+        raw_transaction += &hex_to_little_endian(&prev_index);
+
+        let scriptsig_length = input["scriptsig"].as_str().unwrap().len() / 2;
+        let scriptsig_length_hex = format!("{:02x}", scriptsig_length);
+        raw_transaction += &hex_to_little_endian(&scriptsig_length_hex);
+
+        let sequence_decimal = data["vin"][ind]["sequence"].as_u64().unwrap_or_default();
+        let sequence_hex = format!("{:08x}", sequence_decimal);
+
+        raw_transaction += &hex_to_little_endian(&sequence_hex);
+        
+        ind = ind + 1;
+    }
+
+    let output_count = format!("{:02x}", data["vout"].as_array().unwrap().len());
+    raw_transaction += &hex_to_little_endian(&output_count);
+
+    for output in data["vout"].as_array().unwrap() {
+        let value = format!("{:016x}", (output["value"].as_f64().unwrap()) as u64);
+        raw_transaction += &hex_to_little_endian(&value);
+
+        let script_length = output["scriptpubkey"].as_str().unwrap().len() / 2;
+        let script_length_hex = format!("{:02x}", script_length);
+        raw_transaction += &hex_to_little_endian(&script_length_hex);
+
+        let script_pubkey = output["scriptpubkey"].as_str().unwrap();
+        raw_transaction += script_pubkey;
+    }
+
+    let locktime = format!("{:08x}", data["locktime"].as_u64().unwrap());
+    raw_transaction += &hex_to_little_endian(&locktime);
+    raw_transaction
+}
+
+
+
+
+fn create_transaction_p2pkh(data: serde_json::Value, parameter: usize) -> String {
     let mut raw_transaction = String::new();
 
     let version = format!("{:08x}", data["version"].as_u64().unwrap());
@@ -192,6 +245,63 @@ fn create_transaction_p2pkh(data: serde_json::Value, parameter: usize, flag: &mu
     hex
 }
 
+fn create_transaction_p2pkh_final(data: serde_json::Value) -> String {
+    let mut raw_transaction = String::new();
+
+    let version = format!("{:08x}", data["version"].as_u64().unwrap());
+    raw_transaction += &hex_to_little_endian(&version);
+
+    let input_count = format!("{:02x}", data["vin"].as_array().unwrap().len());
+    raw_transaction += &hex_to_little_endian(&input_count);
+
+    let mut ind: usize = 0;
+
+    for input in data["vin"].as_array().unwrap() {
+        
+        // println!("index is {}",ind);
+        let prev_txid = input["txid"].as_str().unwrap();
+        raw_transaction += &hex_to_little_endian(prev_txid);
+
+        let prev_index = format!("{:08x}", input["vout"].as_u64().unwrap());
+        raw_transaction += &hex_to_little_endian(&prev_index);
+
+        let scriptsig_length = input["scriptsig"].as_str().unwrap().len() / 2;
+        let scriptsig_length_hex = format!("{:02x}", scriptsig_length);
+        raw_transaction += &hex_to_little_endian(&scriptsig_length_hex);
+
+        let scriptsig = input["scriptsig"].as_str().unwrap();
+        raw_transaction += scriptsig;
+
+        let sequence_decimal = data["vin"][ind]["sequence"].as_u64().unwrap_or_default();
+        let sequence_hex = format!("{:08x}", sequence_decimal);
+
+        raw_transaction += &hex_to_little_endian(&sequence_hex);
+        
+        ind = ind + 1;
+    }
+
+    let output_count = format!("{:02x}", data["vout"].as_array().unwrap().len());
+    raw_transaction += &hex_to_little_endian(&output_count);
+
+    for output in data["vout"].as_array().unwrap() {
+        let value = format!("{:016x}", (output["value"].as_f64().unwrap()) as u64);
+        raw_transaction += &hex_to_little_endian(&value);
+
+        let script_length = output["scriptpubkey"].as_str().unwrap().len() / 2;
+        let script_length_hex = format!("{:02x}", script_length);
+        raw_transaction += &hex_to_little_endian(&script_length_hex);
+
+        let script_pubkey = output["scriptpubkey"].as_str().unwrap();
+        raw_transaction += script_pubkey;
+    }
+
+    let locktime = format!("{:08x}", data["locktime"].as_u64().unwrap());
+    raw_transaction += &hex_to_little_endian(&locktime);
+    raw_transaction
+}
+
+
+
 fn main() {
     let folder_path = "../mempool";
     let mut invalid=0;
@@ -223,9 +333,9 @@ fn main() {
                 }
 
                 let hex = if script_sigtype == "p2pkh" {
-                    create_transaction_p2pkh(data.clone(), i, &mut flag)
+                    create_transaction_p2pkh(data.clone(), i)
                 } else if script_sigtype == "v0_p2wpkh" {
-                    create_transaction_p2wpkh(data.clone(), i, &mut flag)
+                    create_transaction_p2wpkh(data.clone(), i)
                 } else {
                     flag = true;
                     break;
@@ -289,6 +399,35 @@ fn main() {
             
             if flag == false {
                 // println!("Transaction is valid");
+                let type_of_transaction = data["vin"][0]["prevout"]["scriptpubkey_type"].as_str().unwrap().to_string();
+                let hex = if type_of_transaction == "p2pkh" {
+                    create_transaction_p2pkh_final(data.clone())
+                } else if type_of_transaction == "v0_p2wpkh" {
+                    create_transaction_p2wpkh_final(data.clone())
+                } else {
+                    flag = true;
+                    break;
+                };
+                // let hex = create_transaction_p2pkh_final(data.clone());
+                let hash_hex: &str = hex.as_str();
+                let data1 = Vec::from_hex(hash_hex).unwrap();
+                let data2 = Sha256::digest(&data1).to_vec();
+                let hash_hex1 = Sha256::digest(&data2).to_vec();
+                let hex = hex::encode(hash_hex1);
+                println!("Hash of the block is: {}",hex);
+                let little_endian = hex_to_little_endian(&hex);
+                if type_of_transaction == "p2pkh" {
+                    println!("Type of the block is: P2PKH");
+                }
+                else if type_of_transaction == "v0_p2wpkh"{
+                    println!("Type of the block is: P2WPKH");
+                }
+                println!("little of the block is: {}",little_endian);
+
+                // let mut f = File::create("../block/".to_string() + &hex + ".json").unwrap();
+
+
+
                 count = count + 1;
             }
             else {
